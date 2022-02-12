6deae105ad5a6bd201718d4bcb744f56032c862a
Akisan <akisan.98@gmail.com>
2022-02-12 18:06:52 +0100

    Main Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { PostPreviewComponent } from './post-preview/post-preview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    PostPreviewComponent
   ],
   imports: [
     BrowserModule
