c5e6281087ffe21ebf5664657a89f61560b9600d
Akisan <51326806+Akisan98@users.noreply.github.com>
2022-03-26 11:23:34 +0100

    FEAT: CONFIG

--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -1,23 +1,31 @@
-// const contentful = require('contentful')
+const contentful = require('contentful')
 
-// exports.handler = async function() {
-//     const client = contentful.createClient({
-//         space: process.env.SPACE_ID,
-//         // environment: '<environment_id>', // defaults to 'master' if not set
-//         accessToken: process.env.CONTENT_DELIVERY_API,
-//     });
+exports.handler = async function() {
+    const client = contentful.createClient({
+        space: process.env.SPACE_ID,
+        // environment: '<environment_id>', // defaults to 'master' if not set
+        accessToken: process.env.CONTENT_DELIVERY_API,
+    });
         
-//     var res = await client.getEntry({ content_type: 'blogConfiguration' })
-//             .then((response) => {
-//                 return response.items
-//             })
-//             .catch(console.error) 
-
-//     return {
-//         statusCode: 200,
-//         body: JSON.stringify(res)
-//     }
-// }
+    return await client.getEntry({ content_type: 'blogConfiguration' })
+        .then((response) => {
+            return {
+                statusCode: 200,
+                body: JSON.stringify(response)
+            }
+        })
+        .catch((error) => {
+            var body = JSON.parse(error.message);
+            return {
+                statusCode: body.status,
+                body: JSON.stringify({
+                    code: body.status,
+                    status: body.statusText,
+                    userMessage: body.message
+                }),
+            };
+        }); // asynchronous, returns promise
+}
 
 
 
